cdc69e0eb6bf11cdd1a14e7fca2d87694adb2c63
90J00319 <90J00319@PASA-WKS-PA102.pasa.corp>
2016-10-19 10:11:34 -0200

    Inclusão de projeto funcional utilizando SpringMVC e AngularJS

---
+++ src/main/java/br/com/angular/model/ResultadoModel.java
@@ -0,0 +1,24 @@
+package br.com.angular.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResultadoModel {
+ 
+	private int codigo;
+	private String mensagem;
+ 
+ 
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h1>Spring MVC e AngularJS</h1>
+<br/>
+<br/>
+<ul style="list-style-type:square">
+  <li><a href="usuario/cadastrar.html">Cadastrar</a></li>
+  <li><a href="usuario/consultarRegistros.html">Consultar</a></li>  
+</ul>
+ 
+</body>
+</html>
